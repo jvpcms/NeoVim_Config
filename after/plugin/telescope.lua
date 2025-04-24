34ab676706952a64c39cc891350d9f48e818257f
jvpcms <jvpcms.engenharia@gmail.com>
2025-04-24 15:21:03 -0300

    telescope show gitignored

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
 
 -- Fuzy find: Files in project, Git files, live grep
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        no_ignore = true,                   -- don't respect .gitignore
+        no_ignore_parent = true,            -- don't respect parent .gitignore
+    })
+end, { desc = 'Find all files (even gitignored)' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
