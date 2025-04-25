c10fcb5bfffef0bd488e5f5458bbb4521e9ea32b
jvpcms <jvpcms.engenharia@gmail.com>
2025-04-24 22:42:20 -0300

    lualine

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -49,4 +49,9 @@ return require('packer').startup(function(use)
     -- GitHub Copilot
     use("github/copilot.vim")
 
+    -- Lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
