920174297553cd9ac0f804f19b586e7d1afd1f8a
jvpcms <jvpcms.engenharia@gmail.com>
2025-05-03 00:04:16 -0300

    close neo tree when open files

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -2,12 +2,31 @@ local neotree = require("neo-tree")
 
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle float<CR>")
 
--- Show hidden files by default
-neotree.setup {
+neotree.setup({
     filesystem = {
         filtered_items = {
             visible = true,
+            hide_dotfiles = false,
             hide_gitignored = true,
         },
+        follow_current_file = {
+            enabled = true,
+        },
+        use_libuv_file_watcher = true,
     },
-}
+    window = {
+        mappings = {
+            ["l"] = "open",
+            ["<CR>"] = "open",
+        },
+    },
+    event_handlers = {
+        {
+            event = "file_open_requested",
+            handler = function()
+                require("neo-tree.command").execute({ action = "close" })
+            end
+        },
+
+    }
+})
