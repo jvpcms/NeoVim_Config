22b0236b1d002b0dd4c913a0d9d15157d80cdb10
jvpcms <jvpcms.engenharia@gmail.com>
2025-05-02 23:56:26 -0300

    remove mapping to netrw

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,3 @@
--- Remap Explore command
-vim.keymap.set("n", "<leader>ee", "<cmd>Lexplore<CR>")
-
 -- Move selected section
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
