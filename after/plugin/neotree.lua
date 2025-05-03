c9e8e4b390633414b53afae816dd952a400e1710
jvpcms <jvpcms.engenharia@gmail.com>
2025-05-03 00:35:20 -0300

    indicate harpoon files

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -4,15 +4,53 @@ vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle float<CR>")
 
 neotree.setup({
     filesystem = {
+
         filtered_items = {
             visible = true,
             hide_dotfiles = false,
             hide_gitignored = true,
         },
+
         follow_current_file = {
             enabled = true,
         },
+
         use_libuv_file_watcher = true,
+
+        -- Indicate files marked by Harpoon
+
+        components = {
+            harpoon_index = function(config, node, _)
+                local harpoon_list = require("harpoon"):list()
+                local path = node:get_id()
+                local harpoon_key = vim.uv.cwd()
+
+                for i, item in ipairs(harpoon_list.items) do
+                    local value = item.value
+                    if string.sub(item.value, 1, 1) ~= "/" then
+                        value = harpoon_key .. "/" .. item.value
+                    end
+
+                    if value == path then
+                        vim.print(path)
+                        return {
+                            text = string.format(" → %d", i), -- <-- Add your favorite harpoon like arrow here
+                            highlight = config.highlight or "NeoTreeDirectoryIcon",
+                        }
+                    end
+                end
+                return {}
+            end,
+        },
+        renderers = {
+            file = {
+                { "icon" },
+                { "name", use_git_status_colors = true },
+                { "harpoon_index" }, --> This is what actually adds the component in where you want it
+                { "diagnostics" },
+                { "git_status", highlight = "NeoTreeDimText" },
+            },
+        },
     },
     window = {
         mappings = {
@@ -28,5 +66,6 @@ neotree.setup({
             end
         },
 
-    }
+    },
+
 })
