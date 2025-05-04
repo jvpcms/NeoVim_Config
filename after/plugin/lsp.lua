cc4e1c79e25af22716e7eec07779219bcb1ba4f9
jvpcms <jvpcms.engenharia@gmail.com>
2025-05-03 21:58:02 -0300

    lualine

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,62 +6,62 @@ local vue_language_server = mason_registry.get_package("vue-language-server")
 local vue_language_server_path = vue_language_server:get_install_path() .. "/node_modules/@vue/language-server"
 
 lspconfig.lua_ls.setup({
-    capabilities = lsp_capabilities,
-    settings = {
-        Lua = {
-            runtime = { version = "LuaJIT" },
-            workspace = {
-                checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true)
-            },
-            telemetry = { enable = false },
-            diagnostics = { globals = { "vim" }, },
-        }
-    }
+	capabilities = lsp_capabilities,
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			workspace = {
+				checkThirdParty = false,
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = { enable = false },
+			diagnostics = { globals = { "vim" } },
+		},
+	},
 })
 
 lspconfig.pyright.setup({
-    capabilities = lsp_capabilities,
-    settings = {
-        python = {
-            analysis = {
-                typeCheckingMode = "basic",
-                autoImportCompletions = true,
-                useLibraryCodeForTypes = true,
-            }
-        }
-    }
+	capabilities = lsp_capabilities,
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic",
+				autoImportCompletions = true,
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 })
 
 lspconfig.ts_ls.setup({
-    capabilities = lsp_capabilities,
-    init_options = {
-        plugins = {
-            {
-                name = "@vue/typescript-plugin",
-                location = vue_language_server_path,
-                languages = { "vue" },
-            }
-        }
-    },
-    filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+	capabilities = lsp_capabilities,
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+			},
+		},
+	},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 })
 
 -- Set up LSP keymaps when LSP attaches to a buffer
-local lspAucmdGroup = vim.api.nvim_create_augroup('UserLspConfig', {})
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = lspAucmdGroup,
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+local lspAucmdGroup = vim.api.nvim_create_augroup("UserLspConfig", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = lspAucmdGroup,
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-        -- LSP keymaps
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'grn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', 'grr', vim.lsp.buf.references, opts)
-    end,
+		-- LSP keymaps
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "grr", vim.lsp.buf.references, opts)
+	end,
 })
 
 -- Set an lsp restart keymap
@@ -69,17 +69,17 @@ vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>", { desc = "Restart LSP" 
 
 -- Diagnostics configuration
 vim.diagnostic.config({
-    virtual_text = {
-        spacing = 4,
-        prefix = "■",
-    },
-    severity_sort = true,
+	virtual_text = {
+		spacing = 4,
+		prefix = "■",
+	},
+	severity_sort = true,
 })
 
 -- Diagnostic highlight
-vim.cmd [[
+vim.cmd([[
   highlight DiagnosticVirtualTextError guifg=#FB4934 guibg=#353535
   highlight DiagnosticVirtualTextWarn guifg=#FABD2F guibg=#353535
   highlight DiagnosticVirtualTextInfo guifg=#8EC07C guibg=#353535
   highlight DiagnosticVirtualTextHint guifg=#83A598 guibg=#353535
-]]
+]])
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,22 +1,23 @@
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use("wbthomason/packer.nvim")
 
     -- Telescope Fuzy Finder
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
     -- Harpoon File Navigation
-    use {
+    use({
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
-        requires = { { "nvim-lua/plenary.nvim" } }
-    }
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
     -- File Explorer
     use({
@@ -24,38 +25,43 @@ return require('packer').startup(function(use)
         branch = "v3.x",
         requires = {
             "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
+        },
+    })
+
+    -- Status Line
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
     })
 
     -- Tresitter Highlighting
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
     -- LSP and Completion
-    use {
+    use({
         "neovim/nvim-lspconfig",
         requires = {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "hrsh7th/nvim-cmp",
             "hrsh7th/cmp-nvim-lsp",
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-vsnip',
-            'hrsh7th/vim-vsnip'
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip",
         },
-    }
+    })
 
     -- Formating
-    use {
-        'nvimtools/none-ls.nvim',
+    use({
+        "nvimtools/none-ls.nvim",
         requires = {
-            'jay-babu/mason-null-ls.nvim'
-        }
-    }
+            "jay-babu/mason-null-ls.nvim",
+        },
+    })
 
     -- LazyGit
     use({
@@ -67,13 +73,13 @@ return require('packer').startup(function(use)
     })
 
     -- Git Signs
-    use('lewis6991/gitsigns.nvim')
+    use("lewis6991/gitsigns.nvim")
 
     -- GitHub Copilot
     use("github/copilot.vim")
 
     -- Undo Tree
-    use('mbbill/undotree')
+    use("mbbill/undotree")
 
     -- Leap
     use("ggandor/leap.nvim")
@@ -82,6 +88,6 @@ return require('packer').startup(function(use)
     use("tpope/vim-surround")
 
     -- Colorschemes
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-    use({ 'sainnhe/gruvbox-material', as = 'gruvbox-material' })
+    use({ "rose-pine/neovim", as = "rose-pine" })
+    use({ "sainnhe/gruvbox-material", as = "gruvbox-material" })
 end)
